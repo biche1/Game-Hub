19dd15cbcea63d6256c526ef17d99c64b71e032a
NavyUndead <Bichechi101@gmail.com>
2024-06-09 15:17:19 +0100

    filtering game by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,15 @@
 import { SimpleGrid, Text, Textarea } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import { Genre } from '../hooks/UseGenres';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+interface Props {
+  selectedGenre: Genre | null
+}
+const GameGrid = ({ selectedGenre}: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6];
     
     return (
@@ -13,8 +17,8 @@ const GameGrid = () => {
             {error && <Text>{error}</Text>}
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' spacing={3}> 
           {isLoading &&
-            skeletons.map((skeleton) =><GameCardContainer> <GameCardSkeleton key={skeleton} /> </GameCardContainer>)}
-          {data.map((data) => <GameCardContainer> <GameCard key={data.id} game={data} /> </GameCardContainer>)}
+            skeletons.map((skeleton) =><GameCardContainer  key={skeleton}> <GameCardSkeleton /> </GameCardContainer>)}
+          {data.map((data) => <GameCardContainer key={data.id}> <GameCard  game={data} /> </GameCardContainer>)}
           </SimpleGrid>
             </>
   )
